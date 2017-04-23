4543ed57e23aed1c216a9213ed3f56069fdf7c2c
caolei.cl <caolei.cl@alibaba-inc.com>
2017-04-23 13:18:25 +0800

    add spring boot  structure

---
+++ src/main/java/com/hackx/web/HackFamilyApplication.java
@@ -0,0 +1,14 @@
+package com.hackx.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class HackFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackFamilyApplication.class, args);
+    }
+}
